98a104a63c3e79054ca15db9ae868bf149fb32f2
hg-tejasmatale <tejas.matale@harbingergroup.com>
2026-02-07 10:29:49 +0530

    Added week 5

---
+++ Week 5/Data Bricks/flights_stats.py
@@ -0,0 +1,11 @@
+from pyspark import pipelines as dp
+from pyspark.sql.functions import *
+
+@dp.table
+def flights_stats():
+    df = spark.read.table("ingest_flights")
+    return df.agg(
+        count("*").alias('num_events'),
+        countDistinct('icao24').alias('distinct_aircraft'),
+        max('velocity').alias('max_velocity'),
+    )
