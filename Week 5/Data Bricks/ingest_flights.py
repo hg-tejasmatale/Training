4023735fb7eade3fc52ab438cd9864d3f3e4a302
hg-tejasmatale <tejas.matale@harbingergroup.com>
2026-02-16 22:21:37 +0530

    Stop tracking .env files

--- Week 5/Data Bricks/ingest_flights.py
+++
@@ -1,8 +0,0 @@
-from pyspark import pipelines as dp
-from pyspark_datasources import OpenSkyDataSource
-spark.dataSource.register(OpenSkyDataSource)
- 
-@dp.table
-def ingest_flights():
-    return spark.readStream.format("opensky").load()
- 
